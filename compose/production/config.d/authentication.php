94b48d12d885e5f09a5e3256e5ff8c41a6c34b5a
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-30 18:55:34 -0300

    configuraçao do autenticador

--- compose/production/config.d/authentication.php
+++ compose/production/config.d/authentication.php
@@ -10,7 +10,23 @@ return [
     'auth.provider' => '\MultipleLocalAuth\Provider',
     'auth.config' => array(
         'salt' => env('AUTH_SALT', null),
-        'timeout' => '24 hours',
+        'timeout' => env('AUTH_TIMEOUT', '24 hours'),
+
+        'enableLoginByCPF' => env('AUTH_LOGIN_BY_CPF', true),
+        'passwordMustHaveCapitalLetters' => env('AUTH_PASS_CAPITAL_LETTERS', true),
+        'passwordMustHaveLowercaseLetters' => env('AUTH_PASS_LOWERCASE_LETTERS', true),
+        'passwordMustHaveSpecialCharacters' => env('AUTH_PASS_SPECIAL_CHARS', true),
+        'passwordMustHaveNumbers' => env('AUTH_PASS_NUMBERS', true),
+        'minimumPasswordLength' => env('AUTH_PASS_LENGTH', 6),
+
+        'google-recaptcha-secret' => env('GOOGLE_RECAPTCHA_SECRET', false),
+        'google-recaptcha-sitekey' => env('GOOGLE_RECAPTCHA_SITEKEY', false),
+
+        'sessionTime' => env('AUTH_SESSION_TIME', 7200), // int , tempo da sessao do usuario em segundos
+        'numberloginAttemp' => env('AUTH_NUMBER_ATTEMPTS', 5), // tentativas de login antes de bloquear o usuario por X minutos
+        'timeBlockedloginAttemp' => env('AUTH_BLOCK_TIME', 900), // tempo de bloqueio do usuario em segundos
+
+
         'strategies' => [
            'Facebook' => array(
                'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
@@ -29,12 +45,10 @@ return [
                 'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
                 'redirect_uri' => $_APP_BASE_URL . 'autenticacao/google/oauth2callback',
                 'scope' => env('AUTH_GOOGLE_SCOPE', 'email'),
-            ),
-            'Twitter' => array(
-                'app_id' => env('AUTH_TWITTER_APP_ID', null),
-                'app_secret' => env('AUTH_TWITTER_APP_SECRET', null),
-            ),
+            )
 
         ]
     ),
 ];
+
+

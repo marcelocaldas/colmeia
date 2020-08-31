212a6c8b55e119d4632a741350e535f74504ca1b
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-08-30 22:38:25 -0300

    configurações dos plugins AldirBlanc e AldirBlancRedirects

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     'plugins' => [
         'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical'],
@@ -7,6 +6,32 @@ return [
         'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
         
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
-        'AldirBlanc' => ['namespace' => 'AldirBlanc'],
+        'AldirBlanc' => [
+            'namespace' => 'AldirBlanc',
+            'config' => [
+                'inciso1_enabled' => true,
+                'inciso2_enabled' => true,
+                'project_id' => 3,
+                'inciso1_opportunity_id' => 9,
+                'inciso2_opportunity_ids' => []
+            ],
+        ],
+        'AldirBlancRedirects' => [
+            'namespace' => 'AldirBlancRedirects',
+            'config' => [
+                'condition' => function() {
+                    $app = MapasCulturais\App::i();
+
+                    // só pode acessar as demais urls quem tiver controle sobre o agente da SECULT
+                    $secult = $app->repo('Agent')->find(env('SECULT_AGENT_ID', 7));
+                    if ($secult) {
+                        return !$secult->canUser('@control');
+                    } else {
+                        return false;
+                    }
+
+                }
+            ]
+        ],
     ]
 ];

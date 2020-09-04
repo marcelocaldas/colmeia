b581ded84ce331e74fa2f1a77818676654d0de4a
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-09-03 21:31:53 -0300

    habilita a rota de busca de CEPs

--- plugins/AldirBlancRedirects/Plugin.php
+++ plugins/AldirBlancRedirects/Plugin.php
@@ -10,12 +10,19 @@ class Plugin extends \MapasCulturais\Plugin {
     }
 
     public function register() {
-        
         $app = App::i();
         $app->_config['routes']['default_controller_id'] = 'aldirblanc';
         
         $config = $this->config;
         $app->hook('GET(<<*>>):before', function() use($app, $config) {
+            $enabled_routes = [
+                ['site', 'address_by_postalcode']
+            ];
+
+            if (in_array([$this->id, $this->action], $enabled_routes)) {
+                return;
+            }
+            
             if(!in_array($this->id, ['aldirblanc', 'auth'])){
                 if(!$config['condition']()){
                     $app->redirect($app->createUrl('aldirblanc', 'index'));

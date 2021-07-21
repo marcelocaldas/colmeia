86ce5e2379540424336acb3be98d5a21de0a596f
erleibiazzio <erleibiazzio@gmail.com>
2021-07-21 09:18:55 -0300

    Altera ID da configuração mediados_owner Ref.: #48

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -160,7 +160,7 @@ return [
                 'link_suporte' => 'https://tawk.to/chat/5f47c17bcc6a6a5947af53ba/default',
                 'msg_inciso2_disabled' => 'A solicitação deste benefício será lançada em breve. Acompanhe a divulgação pelas instituições responsáveis pela gestão da cultura em seu município!',
                 'prefix_project' =>  'Lei Aldir Blanc | ',
-                'mediados_owner' => 7036,
+                'mediados_owner' => 40636,
                 'mediadores_prolongar_tempo' => true,
                 'homologacao_requer_validacao' => ['dataprev'],
                 'exporta_desbancarizados' => [

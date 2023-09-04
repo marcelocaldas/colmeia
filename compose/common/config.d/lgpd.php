b43d6bf9ed7214039d10b022c58cd8cf761dd948
erleibiazzio <erleibiazzio@gmail.com>
2023-09-04 17:55:01 -0300

    Faz com que os textos dos botões dos termos de uso e privacidade sejam exibidos

--- compose/common/config.d/lgpd.php
+++ compose/common/config.d/lgpd.php
@@ -1,17 +1,23 @@
-<?php 
+<?php
+
+use MapasCulturais\i;
+
 return [
     'module.LGPD' => [
         'termsOfUsage'=>[
-            'title'=> 'Termos de Uso', 
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html')
+            'title'=> i::__('Termos de Uso'), 
+            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/terms-of-usage.html'),
+            'buttonText' => i::__('Aceito os termos de uso')
         ],
         'privacyPolicy' => [
-            'title'=>  'Política de Privacidade',
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/privacy-policy.html')
+            'title'=>  i::__('Política de Privacidade'),
+            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/privacy-policy.html'),
+            'buttonText' => i::__('Aceito as políticas de privacidade')
         ],
         'imageOfUsage' => [
-            'title'=>  'Autorização de uso de imagem',
-            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/image-of-usage.html')
+            'title'=>  i::__('Autorização de uso de imagem'),
+            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/image-of-usage.html'),
+            'buttonText' => i::__('Autorizo o uso de imagem')
         ],
     ]
 ];

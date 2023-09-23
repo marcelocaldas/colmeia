c436b0d79433a7f224c4d32b8333bb15b0f1a4fb
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-09-22 21:59:24 -0300

    novas urls de oportunidades da paulo gustavo

--- compose/common/config.d/routes.php
+++ compose/common/config.d/routes.php
@@ -1,25 +1,36 @@
 <?php
 $routes = $config['routes'];
 
-$routes['shortcuts']['lpg/audiovisual'] = ['opportunity', 'single', [1086]];
-$routes['shortcuts']['lpg/musica'] = ['opportunity', 'single', [1084]];
-$routes['shortcuts']['lpg/circo'] = ['opportunity', 'single', [1082]];
-$routes['shortcuts']['lpg/danca'] = ['opportunity', 'single', [1080]];
-$routes['shortcuts']['lpg/teatro'] = ['opportunity', 'single', [1078]];
-$routes['shortcuts']['lpg/cultura-alimentar'] = ['opportunity', 'single', [1076]];
-$routes['shortcuts']['lpg/culturas-populares'] = ['opportunity', 'single', [1074]];
-$routes['shortcuts']['lpg/museus-e-memoriais'] = ['opportunity', 'single', [1072]];
-$routes['shortcuts']['lpg/patrimonio-material'] = ['opportunity', 'single', [1070]];
-$routes['shortcuts']['lpg/patrimonio-imaterial'] = ['opportunity', 'single', [1068]];
-$routes['shortcuts']['lpg/moda-e-design'] = ['opportunity', 'single', [1066]];
-$routes['shortcuts']['lpg/livro-e-leitura'] = ['opportunity', 'single', [1064]];
-$routes['shortcuts']['lpg/culturas-indigenas'] = ['opportunity', 'single', [1062]];
-$routes['shortcuts']['lpg/cultura-urbana-e-periferica'] = ['opportunity', 'single', [1060]];
-$routes['shortcuts']['lpg/cultura-gospel'] = ['opportunity', 'single', [1058]];
-$routes['shortcuts']['lpg/cultura-afrobrasileira'] = ['opportunity', 'single', [1056]];
-$routes['shortcuts']['lpg/artesanato'] = ['opportunity', 'single', [1054]];
-$routes['shortcuts']['lpg/pontos-e-pontoes'] = ['opportunity', 'single', [1052]];
-$routes['shortcuts']['lpg/cultura-digital'] = ['opportunity', 'single', [1050]];
-$routes['shortcuts']['lpg/artes-visuais'] = ['opportunity', 'single', [1048]];
+$list = [
+    'lpg/audiovisual' => 1086,
+    'lpg/musica' => 1084,
+    'lpg/circo' => 1082,
+    'lpg/danca' => 1080,
+    'lpg/teatro' => 1078,
+    'lpg/cultura-alimentar' => 1076,
+    'lpg/culturas-populares' => 1074,
+    'lpg/museus-e-memoriais' => 1072,
+    'lpg/patrimonio-material' => 1070,
+    'lpg/patrimonio-imaterial' => 1068,
+    'lpg/moda-e-design' => 1066,
+    'lpg/livro-e-leitura' => 1064,
+    'lpg/culturas-indigenas' => 1062,
+    'lpg/cultura-urbana-e-periferica' => 1060,
+    'lpg/cultura-gospel' => 1058,
+    'lpg/cultura-afrobrasileira' => 1056,
+    'lpg/artesanato' => 1054,
+    'lpg/pontos-e-pontoes' => 1052,
+    'lpg/cultura-digital' => 1050,
+    'lpg/artes-visuais' => 1048,
+
+    'lpg/audiovisual-formacao-festivais' => 1122,
+    'lpg/audiovisual-salas-de-cinema' => 1120,
+    'lpg/audiovisual-cineclubes-e-itinerantes' => 1124,
+    
+];
+
+foreach($list as $route => $id) {
+    $routes['shortcuts'][$route] = ['opportunity', 'single', [$id]];
+}
 
 return ['routes' => $routes];

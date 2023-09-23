fb919fd10518fd0d315c4783f6fdb2bae135c3c4
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-09-22 23:06:12 -0300

    ajustes no docker-compose de prod/homolog

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -1,39 +1,11 @@
 <?php
 use MapasCulturais\i;
 
-if (env('REDIS_CACHE', false)) {
-    $redis = new \Redis();
-    $redis->connect('redis');
-    $cache = new \Doctrine\Common\Cache\RedisCache;
-    $cache->setRedis($redis);
-
-    $redis = new \Redis();
-    $redis->connect('redis');
-    $mscache = new \Doctrine\Common\Cache\RedisCache;
-    $mscache->setRedis($redis);
-} else {
-    $cache = new \Doctrine\Common\Cache\ApcuCache;
-    $mscache = new \Doctrine\Common\Cache\ApcuCache;
-}
-
 return [
-    'app.cache' => $cache,
-    'app.mscache' => $mscache,
-    'app.siteName' => env('SITE_NAME', 'Mapa cultural do Pará'),
-    'app.siteDescription' => i::__("O Mapa cultural do Pará é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades"),
+    'app.siteName' => 'Mapa cultural do Pará',
+    'app.siteDescription' => "O Mapa Cultural do Pará é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades",
 
     'themes.active' => "MapasPA",
 
-    'app.lcode' => env('APP_LCODE', 'pt_BR,es_ES'),
-
-    'registration.autosaveTimeout' => env('REGISTRATION_AUTOSAVE_INTERVAL', MINUTE_IN_SECONDS * 1000),
-
-    'doctrine.database' => [
-        'host'           => env('DB_HOST', 'db'),
-        'dbname'         => env('DB_NAME', 'mapas'),
-        'user'           => env('DB_USER', 'mapas'),
-        'password'       => env('DB_PASS', 'mapas'),
-        'server_version' => env('DB_VERSION', 10),
-    ],
-    'ini.set' => json_decode(env('PHP_INI_SET', '[]'))
+    'app.lcode' => 'pt_BR',
 ];
--- docker/common/config.d/sharing.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return[
-    'share.image' => env('SHARE_IMAGE', 'img/share.png'),
-    'share.image_twitter' =>  env('SHARE_IMAGE_TWITTER', 'img/share.png'),
-];

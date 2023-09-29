f895cfaab05daa8a2349bf8a089390a307ed361f
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2023-09-29 16:43:07 -0300

    Adição do banner no header da homepage

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -8,4 +8,7 @@ return [
     'themes.active' => "MapasPA",
 
     'app.lcode' => 'pt_BR',
+    'homeHeader.banner' => 'img/banner-caderno-lpg.jpg',
+    'homeHeader.bannerLink' => 'https://mapacultural.pa.gov.br/files/agent/7/caderno-de-orientacoes-pcac-27-09-2023.pdf',
+    'homeHeader.downloadableLink' => true,
 ];

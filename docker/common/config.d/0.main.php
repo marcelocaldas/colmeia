2781eea4ea889d8f70d9e41a7d31af3b00375c4c
marcelocaldas <38535161+marcelocaldas@users.noreply.github.com>
2023-12-22 10:03:48 -0300

    Update 0.main.php

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -2,17 +2,17 @@
 use MapasCulturais\i;
 
 return [
-    'app.siteName' => 'Mapa cultural do Pará',
-    'app.siteDescription' => "O Mapa Cultural do Pará é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades",
+    'app.siteName' => 'A Colmeia',
+    'app.siteDescription' => "A Colmeia é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos ambientais e oportunidades",
 
     'themes.active' => "MapasPA",
 
     'app.lcode' => 'pt_BR',
-    'homeHeader.banner' => 'img/banner-caderno-lpg.jpg',
-    'homeHeader.bannerLink' => 'https://mapacultural.pa.gov.br/files/agent/7/caderno-de-orientacoes-pcac-27-09-2023.pdf',
+    'homeHeader.banner' => 'img/banner1.jpg',
+    'homeHeader.bannerLink' => 'https://colmeia.redeambiental.net/files/agent/7/texto1.pdf',
     'homeHeader.downloadableLink' => true,
 
-    'homeHeader.secondBanner' => 'img/banner-tributacao.png',
-    'homeHeader.secondBannerLink' => 'https://mapacultural.pa.gov.br/files/project/1278/parecer-impostos.pdf',
+    'homeHeader.secondBanner' => 'img/banner2.png',
+    'homeHeader.secondBannerLink' => 'https://colmeia.redeambiental.net/files/project/1278/texto2.pdf',
     'homeHeader.secondDownloadableLink' => true,
 ];

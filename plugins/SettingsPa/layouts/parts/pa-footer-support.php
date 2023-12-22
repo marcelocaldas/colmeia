5e5a01775d6f3d4aa1c8a9bd3442b899d5464f40
marcelocaldas <38535161+marcelocaldas@users.noreply.github.com>
2023-12-22 12:04:53 -0300

    Update pa-footer-support.php

--- plugins/SettingsPa/layouts/parts/pa-footer-support.php
+++ plugins/SettingsPa/layouts/parts/pa-footer-support.php
@@ -1,4 +1,4 @@
 <div class="pa-footer-support">
-    Precisa de ajuda? <a href="https://bit.ly/3hOQfBz">Clique aqui</a> para falar com nossa equipe de suporte por chat. 
-    Ou envie um email para <a href="mailto:suportemapaculturalpa@gmail.com">suportemapaculturalpa@gmail.com</a>
+    Precisa de ajuda? <a href="https://tawk.to/chat/658580c970c9f2407f828bd4/1hi8nu5ek">Clique aqui</a> para falar com nossa equipe de suporte por chat. 
+    Ou envie um email para <a href="mailto:suportecolmeia@redeambiental.net">suportecolmeia@redeambiental.net</a>
 </div>

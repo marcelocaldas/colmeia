e11469145d3bbff855d889cb46ab7e558f5a12cb
marcelocaldas <38535161+marcelocaldas@users.noreply.github.com>
2023-12-22 11:57:28 -0300

    Create texts.php

---
+++ docker/common/texts.php
@@ -0,0 +1,10 @@
+<?php
+use MapasCulturais\i;
+
+return [
+    'agentText' => i::__('Neste espaço, estão registrados ativistas, gestores e produtores; uma rede de atores envolvidos na luta e na ação ambiental. Você pode cadastrar um ou mais agentes (grupos, coletivos, bandas instituições, empresas, etc.), (...)'),  
+    'opportunityText' => i::__('Faça a sua inscrição ou acesse o resultado de diversas convocatórias como editais, oficinas, prêmios e concursos. Você também pode criar o seu próprio formulário e divulgar uma oportunidade para outros agentes ambientais.'),
+    'projectText' => i::__('Aqui você encontra eventos, mostras, convocatórias e editais criados, além de diversas iniciativas cadastradas pelos usuários da plataforma.'),
+    'eventText' => i::__('Você pode pesquisar eventos culturais nos campos de busca combinada. Como usuário cadastrado, você pode incluir seus eventos na plataforma e divulgá-los gratuitamente. (Mais uma linha aqui pra fechar cinco linhas)'),
+    'spaceText' => i::__('Procure pelos espaços ambientais incluídos na plataforma, acessando os campos de busca combinada que ajudam na precisão de sua pesquisa. Cadastre também os espaços onde desenvolve suas ações em defesa do planeta.'),
+];
